f42d50fe8668a30ea33aae96e92ba24b98c7a010
Dino Omanovic <d.omanovic@tarent.de>
2015-11-29 15:21:58 +0100

    Added basic wikigame-backend spring boot module

---
+++ wikigame-backend/src/main/java/de/wikigame/WikigameBackendApplication.java
@@ -0,0 +1,12 @@
+package de.wikigame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikigameBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikigameBackendApplication.class, args);
+    }
+}
